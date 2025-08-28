ca86b9a37fe334fdb8e3247cb508bb4db2e75735
Your GitHub Username <your-github-email@example.com>
2025-08-28 07:12:21 -0400

    responsive enhancmen 2t

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -33,6 +33,8 @@ function Navbar() {
       // If we're not on the main page, navigate to main page first
       navigate('/', { state: { scrollTo: sectionId } });
     }
+    // Close mobile menu after navigation
+    setMobileMenu(false);
   };
 
   const handleLoginClick = () => {
@@ -116,6 +118,10 @@ function Navbar() {
         onClick={handleLogoClick} 
         style={{cursor: 'pointer'}} 
       />
+      
+      {/* Mobile menu icon - always visible */}
+      <FaBars className='menu-icon' onClick={toggleMenu}/>
+      
       <ul className={mobileMenu?'':'hide-mobile-menu'}>
         <li>
           {location.pathname === '/' ? (
@@ -154,20 +160,17 @@ function Navbar() {
           )}
         </li>
         <li>
-          <a href="/gallery" className="gallery-link">{t('navigation.gallery')}</a>
+          <a href="/gallery" className="gallery-link" onClick={() => setMobileMenu(false)}>{t('navigation.gallery')}</a>
         </li>
         {/* <li>
           <LanguageSwitcher />
         </li> */}
-        <li>
-        <FaBars className='menu-icon' onClick={toggleMenu}/>
-      {showProfile && (
-        <UserProfilePopup user={user} onClose={() => setShowProfile(false)} />
-      )}
-      </li>
         {renderAuthSection()}
       </ul>
 
+      {showProfile && (
+        <UserProfilePopup user={user} onClose={() => setShowProfile(false)} />
+      )}
     </nav>
   );
 };
